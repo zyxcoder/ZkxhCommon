dd9f09bd2e03d149806abc35e830354e9c9db9e9
zhangyuxiang <zyx_coder@163.com>
2024-04-02 15:26:59 +0800

    启动页

---
+++ app/src/main/java/com/gxy/zkxhcommon/Main.kt
@@ -0,0 +1,15 @@
+package com.gxy.zkxhcommon
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author zhangyuxiang
+ * @date 2024/4/2
+ */
+class Main :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
