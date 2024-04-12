fc67ac815cf8715acba25c925b96b92c4a05151f
luohao <365624786@qq.com>
2024-04-12 14:40:19 +0800

    fresco显示方式

--- common/src/main/java/com/gxy/common/utils/PhotoLoadUtils.kt
+++ common/src/main/java/com/gxy/common/utils/PhotoLoadUtils.kt
@@ -2,6 +2,7 @@ package com.gxy.common.utils
 
 import android.net.Uri
 import android.widget.ImageView
+import com.facebook.drawee.view.SimpleDraweeView
 import com.luck.picture.lib.config.PictureMimeType
 import com.luck.picture.lib.entity.LocalMedia
 import com.zyxcoder.mvvmroot.utils.loadImage
@@ -17,5 +18,15 @@ fun photoLoadUtils(imageView: ImageView, result: ArrayList<LocalMedia>) {
         imageView.loadImage(Uri.parse(path))
     } else {
         imageView.loadImage(path)
+    }
+}
+
+fun photoLoadUtils(imageView: SimpleDraweeView, result: ArrayList<LocalMedia>) {
+    val media = result[0]
+    val path = media.availablePath
+    if (PictureMimeType.isContent(path) && !media.isCut && !media.isCompressed) {
+        imageView.setImageURI(Uri.parse(path))
+    } else {
+        imageView.setImageURI(path)
     }
 }
