36eaf4234f992986b9568f8c6db8b49ea99f9181
zhangyuxiang <zyx_coder@163.com>
2024-04-18 09:46:44 +0800

    表单demo

---
+++ app/src/main/java/com/gxy/zkxhcommon/MainViewModel.kt
@@ -0,0 +1,18 @@
+package com.gxy.zkxhcommon
+
+import com.gxy.common.common.table.BaseTableViewModel
+
+/**
+ * @author zhangyuxiang
+ * @date 2024/4/18
+ */
+class MainViewModel :BaseTableViewModel() {
+    override suspend fun otherOperate(vararg params: Any?) {
+    }
+
+    override suspend fun updateTableData(mapParams: Map<String?, Any?>?) {
+    }
+
+    override suspend fun insertTableData(mapParams: Map<String?, Any?>?) {
+    }
+}
