d8945db2200a91410283b11d48aecc54335d62d1
zhangyuxiang <zyx_coder@163.com>
2024-04-19 14:03:58 +0800

    日期转换

--- common/src/main/java/com/gxy/common/ext/StringExt.kt
+++ common/src/main/java/com/gxy/common/ext/StringExt.kt
@@ -1,5 +1,8 @@
 package com.gxy.common.ext
 
+import java.lang.Exception
+import java.text.SimpleDateFormat
+
 /**
  * @author zhangyuxiang
  * @date 2024/3/19
@@ -11,4 +14,19 @@ fun String?.safeToFloat(): Float {
 
 fun String?.getFromIdForServerEntity(): Int {
     return this?.substringBefore("-")?.toIntOrNull() ?: 0
+}
+/**
+ * 日期转换
+ */
+fun String.toFormattedDate(inputFormat: String, outputFormat: String): String {
+    try {
+        val inputDateFormat = SimpleDateFormat(inputFormat)
+        val outputDateFormat = SimpleDateFormat(outputFormat)
+
+        val date = inputDateFormat.parse(this)
+        return outputDateFormat.format(date)
+    } catch (e: Exception) {
+        println("日期格式转换出现异常: ${e.message}")
+        return this // 返回原始日期字符串
+    }
 }
